262ea35fb07eaf903f5df2a3d350b4cacd199e98
Yadu <yadusasankan@gmail.com>
2024-09-22 20:08:20 +0530

    feat(space-wars): add projectiles for enemy ships

--- enemy_ship.py
+++ enemy_ship.py
@@ -1,9 +1,12 @@
 from turtle import _Screen, Turtle
-from game_constants import MIN_Y
 from typing import List
+from random import random
+
+from game_constants import MIN_Y, RENDER_DELAY
+
 
 MOVE_SPEED = 2.5
-PROJECTILE_SPEED = 5
+PROJECTILE_SPEED = 2.5
 MAX_MOVE_COUNTER = 90
 STARTING_POSITION = (0, 240)
 
@@ -48,7 +51,6 @@ class EnemyShip(Turtle):
                 projectile.goto(x=projectile.xcor(), y=ycor)
 
         for index in projectiles_to_remove[::-1]:
-            print("index: ", index)
             self.projectiles[index].clear()
             self.projectiles[index].hideturtle()
             del self.projectiles[index]
@@ -69,3 +71,8 @@ class EnemyShip(Turtle):
         if not (self.move_counter % MAX_MOVE_COUNTER):
             self.move_direction = "left" if self.move_direction == "right" else "right"
             self.move_counter = 1
+
+        if random() < RENDER_DELAY / 15:
+            self.fire()
+
+        self.move_existing_projectiles()
